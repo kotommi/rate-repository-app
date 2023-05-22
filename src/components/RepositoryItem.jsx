f362f58599682ef00e76a241a2fb0ade89e7eb5f
Tommi Koivula <tommi.koivula93@gmail.com>
2023-05-22 17:05:15 +0300

    card view for repo items

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,17 +1,71 @@
-import { View, Text, Image } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
+import Text from "./Text";
 
-const RepositoryItem = ({props}) => {
+const styles = StyleSheet.create({
+    avatarLogo: {
+        width: 50,
+        height: 50,
+    },
+    avatarContainer: {
+        flexDirection: "row",
+    },
+    container: {
+        flexDirection: "row",
+        flexGrow: 1,
+    },
+    infoContainer: {
+        flexGrow: 1,
+    },
+    bodyContainer: {
+        flexDirection: "row",
+        flexGrow: 1,
+        justifyContent: "center",
+    },
+})
+
+const CardHeader = ({ item }) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.avatarContainer}>
+                <Image style={styles.avatarLogo} source={{
+                    uri: item.ownerAvatarUrl,
+                }}>
+                </Image>
+            </View>
+            <View>
+                <Text fontWeight={"bold"}>{item.fullName}</Text>
+                <Text>{item.description}</Text>
+                <Text>{item.language}</Text>
+            </View>
+        </View>
+    )
+}
+
+const CardBody = ({ item }) => {
+    return (
+        <View style={styles.infoContainer}>
+            <View style={styles.bodyContainer}>
+                <Text>Stars</Text>
+                <Text>Forks</Text>
+                <Text>Reviews</Text>
+                <Text>Rating</Text>
+            </View>
+            <View style={styles.bodyContainer}>
+                <Text> {item.stargazersCount > 1000 ? `${Number(item.stargazersCount / 1000).toFixed(1)}k` : item.stargazersCount}</Text>
+                <Text>{item.forksCount > 1000 ? `${Number(item.forksCount / 1000).toFixed(1)}k` : item.forksCount}</Text>
+                <Text>{item.reviewCount}</Text>
+                <Text>{item.ratingAverage}</Text>
+            </View>
+        </View>
+    );
+};
+
+const RepositoryItem = ({ props }) => {
     const item = props.item;
     return (
         <View>
-            <Image source={item.avatar}></Image>
-            <Text>Full name: {item.fullName}</Text>
-            <Text>Description: {item.description}</Text>
-            <Text>Language: {item.language}</Text>
-            <Text>Stars: {item.stargazersCount}</Text>
-            <Text>Forks: {item.forksCount}</Text>
-            <Text>Reviews: {item.reviewCount}</Text>
-            <Text>Rating: {item.ratingAverage}</Text>
+            <CardHeader item={item}></CardHeader>
+            <CardBody item={item}></CardBody>
         </View>
     );
 };
